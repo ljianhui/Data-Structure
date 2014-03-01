e38fddb823753afc2fc83794391f8ebf9e35b93e
ljianhui <ljianhui2012@163.com>
2014-03-01 17:28:56 +0800

    修改stack的实现，使stack类更加健壮

--- Stack/_stack.h
+++ Stack/_stack.h
@@ -1,85 +1,117 @@
 #ifndef _STACK_H_INCLUDED
 #define _STACK_H_INCLUDED
-//#include <iostream>
 
 template<typename T>
-class _stack
+class Stack
 {
 	public:
-		_stack(size_t _capacity = 1):capacity(_capacity),pArry(new T[_capacity]),length(0),pTop(pArry){}
-		~_stack(){delete []pArry;}
-		void ClearStack()
-		{
-            length = 0;
-            pTop = pArry;
-        }
+		Stack(size_t capacity = 8):
+            _capacity(capacity),
+            _arry(new T[_capacity]),
+            _length(0),
+            _top(_arry){}
 
-		bool IsEmpty()const {return length == 0;}
-		size_t GetSize()const {return length;}
-		void Push(T& e);
-		T Pop()
-		{
-		    --length;
-            return *(--pTop);
-		}
-		T& GetTop()const;
-		void vist()const;
+		~Stack();
+		inline void clearStack();
+
+		inline bool isEmpty()const;
+		inline size_t getSize()const;
+		inline size_t getCapacity()const;
+		void push(const T& e);
+		inline T pop();
+		inline const T& getTop()const;
 	private:
-		size_t capacity;
-		T *pArry;
-		size_t length;
-		T *pTop;
+        Stack(const Stack &s){}
+        Stack& operator=(const Stack &s){return *this;}
+
+		size_t _capacity;
+		T *_arry;
+		size_t _length;
+		T *_top;
 };
 
+//template<typename T>
+//Stact<T>::Stack(size_t capacity):
+//    _capacity(capacity),
+//    _arry(new T[_capacity]),
+//    _length(0),
+//    _top(_arry)
+//{
+//}
+
 template<typename T>
-T& _stack<T>::GetTop()const
+Stack<T>::~Stack()
 {
-	if(!IsEmpty())
-		return *(pTop - 1);
-	else
-		return *pTop;
+    delete []_arry;
 }
 
 template<typename T>
-void _stack<T>::Push(T &e)
+const T& Stack<T>::getTop()const
 {
-	++length;
-	if(length < capacity)
+	if(!isEmpty())
+		return *(_top - 1);
+	else
+		return *_top;
+}
+
+template<typename T>
+void Stack<T>::push(const T &e)
+{
+	if(_length < _capacity)
 	{
-		*pTop = e;
-		++pTop;
+		*_top = e;
+		++_top;
 	}
 	else
 	{
-		T *ptmp(new T[2*capacity]);
-		T *const phead(ptmp);
-		T *pT(pArry);
-		for(int i = 0; i < length; ++i)
+		T *tmp(new T[2*_capacity]);
+		T *const head(tmp);
+		T *old(_arry);
+		for(int i = 0; i < _length; ++i)
 		{
-			*ptmp = *pT;
-			++ptmp;
-			++pT;
+			*tmp = *old;
+			++tmp;
+			++old;
 		}
-		capacity *= 2;
-		delete []pArry;
-		*(++ptmp) = e;
-		pTop = ++ptmp;
-		pArry = phead;
+		_capacity *= 2;
+		delete []_arry;
+		_arry = head;
+		*tmp = e;
+		_top = ++tmp;
 	}
-}
-template<typename T>
-void _stack<T>::vist()const
-{
-	T *ptmp(pTop);
-	while(ptmp != pArry)
-	{
-		std::cout<<*(--ptmp)<<" ";
-	}
+	++_length;
 }
 
-void fun()
+template<typename T>
+void Stack<T>::clearStack()
 {
-	return;
+    _length = 0;
+    _top = _arry;
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+    --_length;
+    return *(--_top);
+}
+
+template<typename T>
+bool Stack<T>::isEmpty()const
+{
+    return _length == 0;
+}
+
+template<typename T>
+size_t Stack<T>::getSize()const
+{
+    return _length;
+}
+
+template<typename T>
+size_t Stack<T>::getCapacity()const
+{
+    return _capacity;
 }
 
 #endif // _STACK_H_INCLUDED
